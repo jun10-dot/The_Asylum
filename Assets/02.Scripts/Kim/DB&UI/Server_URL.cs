c47a8a8b4e005020bd34190424b921f7d1ccc523
jun10-dot <wnzgud1012@gmail.com>
2026-01-31 00:34:01 +0900

    Clarify comment about local server access
    Updated comment for clarity regarding local server access.

--- Assets/02.Scripts/Kim/DB&UI/Server_URL.cs
+++ Assets/02.Scripts/Kim/DB&UI/Server_URL.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//제 서버는 로컬이기때문에 팀원과 팀장님이 저의 로컬서버에 접근하기위해서
+//팀원과 팀장님이 저의 로컬서버에 접근하기위해서
 //해당 주소를 적어야 접근가능합니다.
 public static class Server_URL
 {
